db201b9911db55ed0fe38178c9c168c6ed092810
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2015-09-20 19:04:07 +0200

    Added roles to setup kieker

---
+++ roles/setup-kieker-recipes/templates/edge-kieker-control.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+exec &> >(tee -a logs/edge-control.log)
+exec 2>&1
+
+echowithdate() {
+	echo "$(date -u) $0 $1"
+}
+
+echousage() {
+	echowithdate "Usage: $0 ( start | stop )"
+}
+
+if [ "$#" -ne 1 ]; then
+	echousage
+	exit 1
+fi
+
+if [ $1 = "start" ]; then
+	echowithdate "Starting..."
+	if [ -f edge-control.pid ]; then
+		echowithdate "Application is already running"
+		echowithdate "If it is not running, delete edge-control.pid manually"
+		#exit 1
+	fi
+	#java -jar rss-edge/build/libs/rss-edge-*SNAPSHOT.jar &
+	#java -javaagent:kieker-{{ kieker_version }}-aspectj.jar -Dkieker.monitoring.configuration=kieker.monitoring.properties -cp sigar-1.6.4.jar:rss-edge/build/libs/rss-edge-0.1.0-SNAPSHOT.jar com.netflix.recipes.rss.server.EdgeServer &
+	java -javaagent:kieker-{{ kieker_version }}-aspectj.jar -Dkieker.monitoring.configuration=kieker.monitoring.properties -cp rss-edge/build/libs/rss-edge-0.1.0-SNAPSHOT.jar com.netflix.recipes.rss.server.EdgeServer &
+	PID=$!
+	echo $PID > edge-control.pid
+	echowithdate "pid = $PID"
+	echowithdate "Done"
+elif [ $1 = "stop" ]; then
+	echowithdate "Stoping..."
+	if [ ! -f edge-control.pid ]; then
+		echowithdate "Cannot find edge-control.pid"
+		echowithdate "Has the application been started?"
+		#exit 1
+	fi
+	kill $(cat edge-control.pid)
+	rm edge-control.pid
+	echowithdate "Done"
+else
+	echousage
+	exit 1
+fi
---
+++ roles/setup-kieker-recipes/templates/middletier-kieker-control.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+exec &> >(tee -a logs/middletier-control.log)
+exec 2>&1
+
+echowithdate() {
+	echo "$(date -u) $0 $1"
+}
+
+echousage() {
+	echowithdate "Usage: $0 ( start | stop )"
+}
+
+if [ "$#" -ne 1 ]; then
+	echousage
+	exit 1
+fi
+
+if [ $1 = "start" ]; then
+	echowithdate "Starting..."
+	if [ -f middletier-control.pid ]; then
+		echowithdate "Application is already running"
+		echowithdate "If it is not running, delete middletier-control.pid manually"
+		#exit 1
+	fi
+	#java -jar rss-middletier/build/libs/rss-middletier-*SNAPSHOT.jar &
+	java -javaagent:kieker-{{ kieker_version }}-aspectj.jar -Dkieker.monitoring.configuration=kieker.monitoring.properties -cp rss-middletier/build/libs/rss-middletier-0.1.0-SNAPSHOT.jar com.netflix.recipes.rss.server.MiddleTierServer &
+	PID=$!
+	echo $PID > middletier-control.pid
+	echowithdate "pid = $PID"
+	echowithdate "Done"
+elif [ $1 = "stop" ]; then
+	echowithdate "Stoping..."
+	if [ ! -f middletier-control.pid ]; then
+		echowithdate "Cannot find middletier-control.pid"
+		echowithdate "Has the application been started?"
+		#exit 1
+	fi
+	kill $(cat middletier-control.pid)
+	rm middletier-control.pid
+	echowithdate "Done"
+else
+	echousage
+	exit 1
+fi
