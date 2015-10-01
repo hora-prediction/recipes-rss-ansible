c828f5e8f15ffbca496db138e4e20000c842c37d
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2015-10-01 18:19:10 +0200

    Fixed bug when starting kieker resource monitor

--- roles/setup-kieker-resource-monitor/files/kieker-resource-monitor-control.sh
+++ roles/setup-kieker-resource-monitor/files/kieker-resource-monitor-control.sh
@@ -26,7 +26,7 @@ if [ $1 = "start" ]; then
 	BINDIR=$(cd "$(dirname "$0")"; pwd)/
 	JAVAARGS="-Dkieker.common.logging.Log=JDK -Dkieker.common.logging.Log=JDK -Djava.util.logging.config.file=${BINDIR}/logging.properties -Xms56m -Xmx1024m -Dkieker.monitoring.configuration=kieker.monitoring.properties"
 	MAINCLASSNAME=kieker.tools.resourceMonitor.ResourceMonitor
-java ${JAVAARGS} -cp "${BINDIR}/../lib/*":"${BINDIR}/../lib/sigar/*":"${BINDIR}/../build/libs/*":"${BINDIR}" ${MAINCLASSNAME} $*
+	java ${JAVAARGS} -cp "${BINDIR}/../lib/*":"${BINDIR}/../lib/sigar/*":"${BINDIR}/../build/libs/*":"${BINDIR}" ${MAINCLASSNAME} $* &
 	PID=$!
 	echo $PID > kieker-resource-monitor-control.pid
 	echowithdate "pid = $PID"
